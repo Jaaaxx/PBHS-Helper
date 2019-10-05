99f0e50cd83d539fb039d2d99e00aee77ba636ae
Jax Hutton <jax.hutton@outlook.com>
2019-10-04 23:47:14 -0400

    Added assignments screen APP IS READY!!!

--- app/src/main/java/com/browardschools/pbhshelper/AssignmentsActivity.kt
+++ app/src/main/java/com/browardschools/pbhshelper/AssignmentsActivity.kt
@@ -1,17 +1,28 @@
 package com.browardschools.pbhshelper
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log.d
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.res.ResourcesCompat
-
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_assignments.*
+import kotlinx.android.synthetic.main.content_assignments.*
+import org.json.JSONArray
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class AssignmentsActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_assignments)
@@ -20,7 +31,84 @@ class AssignmentsActivity : AppCompatActivity() {
         @SuppressLint("SimpleDateFormat")
         val formatter = SimpleDateFormat("MM/dd")
         val date: String = formatter.format(Date())
+        val density = this.resources.displayMetrics.density
+        val dp = { p: Int -> (p * density).toInt() }
         val mLayout = findViewById<CoordinatorLayout>(R.id.assignment_layout)
+
+        var sessionId = "FALSE"
+        try {
+            sessionId = intent.getStringExtra("CourseRefer")!!
+        } catch (e: NullPointerException) {
+        }
+        if (sessionId != "FALSE") {
+            d("It works", "IT WORKS")
+            val gradesText = getSharedPreferences("GradesSettings", 0).getString("rawGrades", "")
+            if (gradesText != "" && gradesText != null) {
+                runOnUiThread { vertASParent.removeAllViews() }
+                val gradesJSON = JSONArray(gradesText)
+                var assignmentsJSON: JSONArray = JSONArray()
+                for (i in 0 until gradesJSON.length()) {
+                    if (gradesJSON.getJSONObject(i).getString("Course") == sessionId) {
+                        assignmentsJSON = gradesJSON.getJSONObject(i).getJSONArray("Assignments")
+                    }
+                }
+                if (assignmentsJSON.length() != 0) {
+                    for (i in 0 until assignmentsJSON.length()) {
+                        val assignment = assignmentsJSON.getJSONObject(i)
+                        d("assignments name", assignment.getString("Name"))
+                        val horParent = LinearLayout(this)
+                        val lp = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            2f
+                        )
+                        lp.setMargins(dp(10), dp(10), dp(10), dp(0))
+                        horParent.layoutParams = lp
+                        horParent.orientation = LinearLayout.HORIZONTAL
+
+                        val txtViewChild = TextView(this)
+                        txtViewChild.layoutParams = LinearLayout.LayoutParams(
+                            dp(0),
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            2f
+                        )
+                        txtViewChild.typeface =
+                            Typeface.create("sans-serif-condensed-medium", Typeface.NORMAL)
+                        txtViewChild.gravity = Gravity.CENTER_VERTICAL
+                        txtViewChild.text = assignment.getString("Name")
+                        txtViewChild.textSize = 20.0f
+
+                        val valTV = TextView(this)
+                        valTV.layoutParams = LinearLayout.LayoutParams(
+                            dp(120),
+                            LinearLayout.LayoutParams.MATCH_PARENT
+                        )
+                        try {
+                            valTV.text =
+                                "${(assignment.getString("Points").toFloat() / assignment.getString(
+                                    "Max"
+                                ).toFloat() * 100).toInt()}%"
+                        } catch (e: NumberFormatException) {
+                            valTV.text = getString(R.string.null_assignment)
+                        }
+                        valTV.gravity = Gravity.CENTER
+                        valTV.background =
+                            resources.getDrawable(android.R.drawable.editbox_dropdown_light_frame)
+                        valTV.textSize = 30.0f
+                        horParent.addView(txtViewChild)
+                        horParent.addView(valTV)
+                        runOnUiThread { vertASParent.addView(horParent) }
+                    }
+                }
+            }
+        } else {
+            Snackbar.make(
+                findViewById(android.R.id.content),
+                "Please try again.",
+                Snackbar.LENGTH_SHORT
+            ).show()
+        }
+
 
         when (datesDays[date]) {
             1 -> mLayout.setBackgroundColor(
@@ -47,4 +135,20 @@ class AssignmentsActivity : AppCompatActivity() {
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                val intent = Intent(
+                    this@AssignmentsActivity,
+                    GradesActivity::class.java
+                ).putExtra("Referrer", "AssignmentsActivity")
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
--- app/src/main/java/com/browardschools/pbhshelper/GradesActivity.kt
+++ app/src/main/java/com/browardschools/pbhshelper/GradesActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.graphics.Typeface
 import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
@@ -34,41 +35,42 @@ class GradesActivity : AppCompatActivity() {
         val formatter = SimpleDateFormat("MM/dd")
         val date: String = formatter.format(Date())
         findViewById<RelativeLayout>(R.id.loadingPanel).visibility = View.GONE
+
         val mLayout = findViewById<CoordinatorLayout>(R.id.grades_layout)
         val settings = getSharedPreferences("Login", 0)
         val username = settings.getString("user", "")
         val password = settings.getString("pass", "")
         val density = this.resources.displayMetrics.density
         val dp = { p: Int -> (p * density).toInt() }
+        var sessionId = "FALSE"
+        try {
+            sessionId = intent.getStringExtra("Referrer")!!
+            Log.d("sessionID", sessionId)
+        } catch (e: NullPointerException) {
+        }
+        if (sessionId != "AssignmentsActivity") {
+            findViewById<RelativeLayout>(R.id.loadingPanel).visibility = View.VISIBLE
+        }
         AsyncTask.execute {
-            if (getSharedPreferences("GradesSettings", 0).getString("rawGrades", "") == ""
-                || getSharedPreferences("GradesSettings", 0).getString("rawGrades", "") == null
-            ) {
-                runOnUiThread {
-                    findViewById<RelativeLayout>(R.id.loadingPanel).visibility = View.VISIBLE
+            if (sessionId != "AssignmentsActivity") {
+                try {
+                    val gradesText =
+                        URL("https://pinnacle-scraper.herokuapp.com/api?un=$username&pw=$password").readText()
+                    if (gradesText == "Username or Password was Incorrect") {
+                        getSharedPreferences("Login", 0).edit().clear().apply()
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    } else {
+                        getSharedPreferences("GradesSettings", 0).edit()
+                            .putString("rawGrades", gradesText).apply()
+                    }
+                } catch (e: FileNotFoundException) {
+                    Snackbar.make(
+                        findViewById(android.R.id.content),
+                        "Network error. Try again later!",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    getSharedPreferences("GradesSettings", 0).edit().clear().apply()
                 }
-            }
-            try {
-                val gradesText =
-                    URL("https://pinnacle-scraper.herokuapp.com/api?un=$username&pw=$password").readText()
-                if (gradesText == "Username or Password was Incorrect") {
-                    getSharedPreferences("Login", 0).edit().clear().apply()
-                    startActivity(Intent(this, LoginActivity::class.java))
-                } else {
-                    getSharedPreferences("GradesSettings", 0).edit()
-                        .putString("rawGrades", gradesText).apply()
-                }
-            } catch (e: FileNotFoundException) {
-                Snackbar.make(
-                    findViewById(android.R.id.content),
-                    "Network error. Try again later!",
-                    Snackbar.LENGTH_LONG
-                ).show()
-                getSharedPreferences("GradesSettings", 0).edit().clear().apply()
-            }
-            if (getSharedPreferences("GradesSettings", 0).getString("rawGrades", "") == ""
-                || getSharedPreferences("GradesSettings", 0).getString("rawGrades", "") == null
-            ) {
                 runOnUiThread {
                     findViewById<RelativeLayout>(R.id.loadingPanel).visibility = View.GONE
                 }
@@ -85,7 +87,7 @@ class GradesActivity : AppCompatActivity() {
                         val horParent = LinearLayout(this)
                         val lp = LinearLayout.LayoutParams(
                             LinearLayout.LayoutParams.MATCH_PARENT,
-                            dp(0),
+                            dp(85),
                             2f
                         )
                         lp.setMargins(dp(10), dp(0), dp(0), dp(0))
@@ -112,60 +114,17 @@ class GradesActivity : AppCompatActivity() {
                         btnChild.text = i.getString("Grade")
                         btnChild.textSize = 30.0f
                         btnChild.setOnClickListener {
-                            startActivity(Intent(this, AssignmentsActivity::class.java))
+                            startActivity(
+                                Intent(
+                                    this,
+                                    AssignmentsActivity::class.java
+                                ).putExtra("CourseRefer", i.getString("Course"))
+                            )
                         }
                         horParent.addView(txtViewChild)
                         horParent.addView(btnChild)
                         runOnUiThread { vertParent.addView(horParent) }
                     }
-                }
-            }
-        }
-        val gradesText = getSharedPreferences("GradesSettings", 0).getString("rawGrades", "")
-        if (gradesText != "" && gradesText != null) {
-            runOnUiThread { vertParent.removeAllViews() }
-            val gradesJSON = JSONArray(gradesText)
-            val allCourses = ArrayList<JSONObject>()
-            for (i in 0 until gradesJSON.length())
-                allCourses.add(gradesJSON.getJSONObject(i))
-            for (i in allCourses) {
-                if (i.getString("Quarter") == "Quarter 1") {
-                    val horParent = LinearLayout(this)
-                    val lp = LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        dp(0),
-                        2f
-                    )
-                    lp.setMargins(dp(10), dp(0), dp(0), dp(0))
-                    horParent.layoutParams = lp
-                    horParent.orientation = LinearLayout.HORIZONTAL
-
-                    val txtViewChild = TextView(this)
-                    txtViewChild.layoutParams = LinearLayout.LayoutParams(
-                        dp(0),
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        2f
-                    )
-                    txtViewChild.typeface =
-                        Typeface.create("sans-serif-condensed-medium", Typeface.NORMAL)
-                    txtViewChild.gravity = Gravity.CENTER_VERTICAL
-                    txtViewChild.text = i.getString("Course")
-                    txtViewChild.textSize = 30.0f
-
-                    val btnChild = Button(this)
-                    btnChild.layoutParams = LinearLayout.LayoutParams(
-                        dp(120),
-                        LinearLayout.LayoutParams.MATCH_PARENT
-                    )
-                    btnChild.text = i.getString("Grade")
-                    btnChild.textSize = 30.0f
-                    // TODO Same thing as this screen, but for individual class assignments
-                    btnChild.setOnClickListener {
-                        startActivity(Intent(this, AssignmentsActivity::class.java))
-                    }
-                    horParent.addView(txtViewChild)
-                    horParent.addView(btnChild)
-                    runOnUiThread { vertParent.addView(horParent) }
                 }
             }
         }
@@ -175,5 +134,4 @@ class GradesActivity : AppCompatActivity() {
             else -> mLayout.setBackgroundColor(ResourcesCompat.getColor(resources, R.color.shiracha, null))
         }
     }
-
 }
