78a38a28182d77fdfbd234a454689843308137f9
Jax Hutton <jax.hutton@outlook.com>
2019-10-29 22:34:33 -0400

    Fixed InstrumentedTest browardschools

--- app/src/androidTest/java/com/jaxhutton/pbhshelper/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/jaxhutton/pbhshelper/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.browardschools.pbhshelper", appContext.packageName)
+        assertEquals("com.jaxhutton.pbhshelper", appContext.packageName)
     }
 }
